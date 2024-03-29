49220519f79a8dd0e6f84c7705a6d498420a6945
sheep0704 <309933706@qq.com>
2024-03-29 18:22:57 +0800

    修改

--- app/constant.ts
+++ app/constant.ts
@@ -1,4 +1,4 @@
-export const OWNER = "djy1992";
+export const OWNER = "sheep0704";
 export const REPO = "ChatGPT-Next-Web";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
